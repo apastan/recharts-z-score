cb0858c7c9b9aabf76516448f1ad19a3e8226e03
apastan <apastan.dev@gmail.com>
2025-04-23 20:30:32 +0300

    update `base` settings in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/recharts-z-score'
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/recharts-z-score' : '/'
+
+  return {
+    plugins: [react()],
+    base, // Используем динамически заданный base
+  }
 })
